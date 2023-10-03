97433e4f8e22fb39642d10e01e85efc58b6f34c3
farrza10 <70225671+farrza10@users.noreply.github.com>
2023-10-03 16:40:03 +0300

    Update bicycle.py

--- bicycle.py
+++ bicycle.py
@@ -1,36 +1,40 @@
-import datetime
+mport datetime
+
 class BikeRental:
     
     def __init__(self,stock=0):
         """
         Our constructor class that instantiates bike rental shop.
         """
-        self.stock = stock 
+
+        self.stock = stock
+
     def displaystock(self):
         """
         Displays the bikes currently available for rent in the shop.
         """
-        print("We have currently {} bikes available to     rent.".format(self.stock))
+
+        print("We have currently {} bikes available to rent.".format(self.stock))
         return self.stock
+
     def rentBikeOnHourlyBasis(self, n):
         """
         Rents a bike on hourly basis to a customer.
         """
-        # reject invalid input 
         if n <= 0:
             print("Number of bikes should be positive!")
             return None
-        # do not rent bike is stock is less than requested bikes
         
         elif n > self.stock:
             print("Sorry! We have currently {} bikes available to rent.".format(self.stock))
             return None
-        # rent the bikes        
+        
         else:
             now = datetime.datetime.now()                      
             print("You have rented a {} bike(s) on hourly basis today at {} hours.".format(n,now.hour))
             print("You will be charged $5 for each hour per bike.")
             print("We hope that you enjoy our service.")
+
             self.stock -= n
             return now      
      
@@ -38,10 +42,10 @@ class BikeRental:
         """
         Rents a bike on daily basis to a customer.
         """
-        
         if n <= 0:
             print("Number of bikes should be positive!")
             return None
+
         elif n > self.stock:
             print("Sorry! We have currently {} bikes available to rent.".format(self.stock))
             return None
@@ -51,6 +55,7 @@ class BikeRental:
             print("You have rented {} bike(s) on daily basis today at {} hours.".format(n, now.hour))
             print("You will be charged $20 for each day per bike.")
             print("We hope that you enjoy our service.")
+
             self.stock -= n
             return now
         
@@ -61,6 +66,7 @@ class BikeRental:
         if n <= 0:
             print("Number of bikes should be positive!")
             return None
+
         elif n > self.stock:
             print("Sorry! We have currently {} bikes available to rent.".format(self.stock))
             return None        
@@ -71,8 +77,10 @@ class BikeRental:
             print("You will be charged $60 for each week per bike.")
             print("We hope that you enjoy our service.")
             self.stock -= n
+
             return now
     
+
     
     def returnBike(self, request):
         """
@@ -80,11 +88,9 @@ class BikeRental:
         2. Replensihes the inventory
         3. Return a bill
         """
-       
-        # extract the tuple and initiate bill
         rentalTime, rentalBasis, numOfBikes = request
         bill = 0
-        # issue a bill only if all three parameters are not null!
+
         if rentalTime and rentalBasis and numOfBikes:
             self.stock += numOfBikes
             now = datetime.datetime.now()
@@ -102,18 +108,22 @@ class BikeRental:
             elif rentalBasis == 3:
                 bill = round(rentalPeriod.days / 7) * 60 * numOfBikes
             
-            # family discount calculation
+               
             if (3 <= numOfBikes <= 5):
                 print("You are eligible for Family rental promotion of 30% discount")
                 bill = bill * 0.7
+
             print("Thanks for returning your bike. Hope you enjoyed our service!")
             print("That would be ${}".format(bill))
             return bill
-        
         else:
             print("Are you sure you rented a bike with us?")
             return None
+
+
+
 class Customer:
+
     def __init__(self):
         """
         Our constructor method which instantiates various customer objects.
@@ -123,6 +133,7 @@ class Customer:
         self.rentalBasis = 0
         self.rentalTime = 0
         self.bill = 0
+
     
     def requestBike(self):
         """
@@ -130,13 +141,12 @@ class Customer:
         """
                       
         bikes = input("How many bikes would you like to rent?")
-        
-        # implement logic for invalid input
         try:
             bikes = int(bikes)
         except ValueError:
             print("That's not a positive integer!")
             return -1
+
         if bikes < 1:
             print("Invalid input. Number of bikes should be greater than zero!")
             return -1
